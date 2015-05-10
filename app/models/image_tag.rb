6abbb2db4bb98666d37cb6c387db42062c01f6d5
Atte Lassila <atte.lassila@cs.helsinki.fi>
2015-05-10 23:49:08 +0300

    Fixed some broken tests

--- app/models/image_tag.rb
+++ app/models/image_tag.rb
@@ -3,4 +3,6 @@ class ImageTag < ActiveRecord::Base
   belongs_to :tag
 
   validates :image_id, :uniqueness => {:scope =>:tag_id}
+  validates :image_id, presence: :true
+  validates :tag_id, presence: :true
 end
